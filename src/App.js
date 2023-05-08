8d38ac19935252390e1f22d3d1d25c98053f9654
glisar <127492864+glisar@users.noreply.github.com>
2023-05-08 21:30:05 +0530

    23 commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
+
   render() {
     return (
+      <>
       <div>
-        helloo
-      </div>
+
+        <NavBar/>
+        <News/>
+        </div>
+      </>
     )
   }
 }
