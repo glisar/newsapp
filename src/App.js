09b1abcde4bfa1b7e3b19648dc4a08a08ddd5a00
glisar <127492864+glisar@users.noreply.github.com>
2023-06-03 22:31:05 +0530

    29 commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
       <div>
 
         <NavBar />
-        <News/>
+        <News pageSize={6}/>
         </div>
       </>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,12 +6,12 @@ export class NewsItem extends Component {
     let {title,description,imageUrl,newsUrl} = this.props;
     return (
       <div className ="container">
-        <div className="card" style={{width: '18rem'}}>
-  <img src={imageUrl} className="card-img-top" alt="..."/>
+        <div className="card" >
+  <img src={!imageUrl?"https://www.reuters.com/resizer/v6e3QgvKWC2y-1nCrtm9aLNuV9M=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/ESB7X3WPYBNETOE5WPQ6BJLA5A.jpg":imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
-    <a href="/newsdetail/" className="btn btn-sm btn-primary">read More</a>
+    <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">read More</a>
   </div>
 </div>
       </div>
